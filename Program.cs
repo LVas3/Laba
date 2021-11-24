946d27d7e2ab8051a0062ef74d266573c186c7f4
Леонид Васильев <Leonvas0304@gmail.com>
2021-11-24 22:45:01 +0300

    1

--- Program.cs
+++ Program.cs
@@ -6,7 +6,12 @@ namespace Laba
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("Привет!");
+            }
+            string nameAnimal = "Баксик";
+            Console.WriteLine($"Я очень люблю своего котика + {nameAnimal}!");
         }
-    }
 }
